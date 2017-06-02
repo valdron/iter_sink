b1e1de1acc7c082d4fd1bd23dce7aa311a5f2020
valdron <seehofer.paul@gmail.com>
2017-06-02 08:37:46 +0200

    test

--- src/lib.rs
+++ src/lib.rs
@@ -1,15 +1,14 @@
 
 use std::sync::mpsc::Sender;
 
-trait Sink<T> 
-{
-    fn drain<I>(&self, iter: I) -> Result<(),()>
-    where I: Iterator<Item=T>;
+trait Sink<T> {
+    fn drain<I>(&self, iter: I) -> Result<(), ()> where I: Iterator<Item = T>;
 }
 
 impl<T> Sink<T> for Sender<T> {
-    fn drain<I>(&self, iter: I) -> Result<(),()>
-    where I: Iterator<Item=T> {
+    fn drain<I>(&self, iter: I) -> Result<(), ()>
+        where I: Iterator<Item = T>
+    {
         for item in iter {
             match self.send(item) {
                 Err(_) => return Err(()),
@@ -21,15 +20,15 @@ impl<T> Sink<T> for Sender<T> {
 }
 
 trait SinkIter<T> {
-    fn sink<S>(self, sink: S) -> Result<(), ()>
-    where S: Sink<T>;
+    fn sink<S>(self, sink: S) -> Result<(), ()> where S: Sink<T>;
 }
 
-impl<T, I> SinkIter<T> for I 
-    where I: Iterator<Item=T>
+impl<T, I> SinkIter<T> for I
+    where I: Iterator<Item = T>
 {
     fn sink<S>(self, sink: S) -> Result<(), ()>
-    where S: Sink<T> {
+        where S: Sink<T>
+    {
         sink.drain(self)
     }
 }
@@ -40,16 +39,16 @@ impl<T, I> SinkIter<T> for I
 
 #[cfg(test)]
 mod tests {
-    use super::{SinkIter};
+    use super::SinkIter;
     use std::sync::mpsc::channel;
     #[test]
     fn it_works() {
         let (tx, rx) = channel();
         let _ = (0..3).sink(tx);
-        
+
 
         let v: Vec<_> = rx.iter().take(3).collect();
-        assert_eq!(v, vec![0,1,2]);
+        assert_eq!(v, vec![0, 1, 2]);
 
     }
 }
